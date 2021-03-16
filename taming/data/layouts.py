79dd001a573dcf57234057fb4ce20ce02d745ca1
Theodore Galanos <teddian@gmail.com>
2021-03-17 00:11:24 +0800

    Create layouts.py

---
+++ taming/data/layouts.py
@@ -0,0 +1,46 @@
+import os
+import numpy as np
+import albumentations
+from torch.utils.data import Dataset
+
+from taming.data.base import ImagePaths, ConcatDatasetWithIndex
+
+class Base(Dataset):
+    def __init__(self, *args, **kwargs):
+        super().__init__()
+        self.data = None
+        self.keys = None
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, i):
+        example = self.data[i]
+        ex = {}
+        if self.keys is not None:
+            for k in self.keys:
+                ex[k] = example[k]
+        else:
+            ex = example
+        return ex
+		
+class LayoutsTrain(Base):
+    def __init__(self, size, keys=None):
+        super().__init__()
+        root = "data/layouts/train"
+        with open("data/layoutsrain.txt", "r") as f:
+            relpaths = f.read().splitlines()
+        paths = [os.path.join(root, relpath) for relpath in relpaths]
+        self.data = ImagePaths(paths=paths, size=size, random_crop=False)
+        self.keys = keys
+
+
+class LayoutsValidation(Base):
+    def __init__(self, size, keys=None):
+        super().__init__()
+        root = "data/layouts/test"
+        with open("data/layoutsvalidation.txt", "r") as f:
+            relpaths = f.read().splitlines()
+        paths = [os.path.join(root, relpath) for relpath in relpaths]
+        self.data = ImagePaths(paths=paths, size=size, random_crop=False)
+        self.keys = keys
